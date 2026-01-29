a670b52f72e462ee8b74c84572591c9606ea8388
frannpineroo <frannpinero@hotmail.com>
2026-01-29 17:30:38 -0300

    Servir contenido estatico

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
+import { join } from 'path'; // Paquete de Node.js para manejar rutas de archivos
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    })
+  ],
   controllers: [],
   providers: [],
 })
