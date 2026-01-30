f3de86d2069e56a0f0a67c1ca0e46b2401dbbfeb
frannpineroo <frannpinero@hotmail.com>
2026-01-30 11:50:55 -0300

    Primer api y global prefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { join } from 'path'; // Paquete de Node.js para manejar rutas de archivos
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-    })
+    }),
+    PokemonModule
   ],
   controllers: [],
   providers: [],
